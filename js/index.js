6c11b664ae90cfb5c651137fc1c7e0e3b3403880
arifkhondokar09 <aimless.arif09@gmail.com>
2025-03-17 00:17:09 +0600

    almost done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,45 @@
 
+function handleButtonClick(buttonId, message) {
+  document.getElementById(buttonId).addEventListener("click", function () {
+     let task = document.getElementById("task");
+      let total = parseInt(task.innerText) - 1;
+      task.innerText = total;
+
+      this.setAttribute("disabled", "true");
+      this.classList.remove("bg-blue-600");
+      this.classList.add("bg-gray-400");
+
+      let btnNumber = document.getElementById("btn-number");
+      let result = parseInt(btnNumber.innerText) + 1;
+      btnNumber.innerText = result;
+
+    let notification = document.getElementById("notification");
+   let para = document.createElement("p");
+      para.innerText = message;
+      notification.appendChild(para);
+      para.classList.add("bg-slate-200", "m-2", "ml-5", "rounded-lg");
+  });
+}
+
+
+handleButtonClick("btn-fix-mobile", "You have completed mobile fix"); 
+handleButtonClick("btn-dark-mode", "You have completed dark mode task");
+handleButtonClick("btn-optimize", "You have completed optimization task");
+handleButtonClick("btn-emoji", "You have completed emoji task");
+handleButtonClick("btn-openai", "You have completed OpenAI task");
+handleButtonClick("btn-job", "You have completed job task");
 
 
 
 
-document.getElementById("btn-fix-mobile").addEventListener("click",function(){
-
-//   let task= document.getElementById("task");
-//  let result = parseInt(task.innerText)-1;
-// document.getElementById("task").innerText= result;
-
-const task= document.getElementById("task");
-const total=parseInt(task.innerText)-1;
-task.innerText= total;
-
-//  this.disabled= true;
-this.setAttribute("disbaled","true");
-
- this.classList.remove("bg-blue-600"); // Remove blue color
- this.classList.add("bg-gray-400","cursor-not-allowed");
 
 
+document.getElementById("btn-clear-history").addEventListener("click", function(){
 
- const btnNumber= document.getElementById("btn-number");
- result=  parseInt(btnNumber.innerText)+1;
- btnNumber.innerText= result;
-
-
- const notification= document.getElementById("notification");
-
- const para = document.createElement("p");
-
-para.innerText="you have completed mobile fix";
-
-notification.appendChild(para);
-para.classList.add("bg-slate-200","m-2","ml-5", "rounded-lg")
+const btnClearHistory= document.getElementById("btn-clear-history");
 
 
 
 
- 
+
 })
